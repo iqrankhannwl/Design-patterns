f3c382f3963f30a6fd4aad84cbaae5cd20d009e9
Ayzal-fatima <iqrankhannwl@gmail.com>
2024-02-03 03:18:02 +0500

    main branch

---
+++ Factory/example-01/lang_client/__init__.py
@@ -0,0 +1,9 @@
+from .chinese import Chiness
+from .english import English
+from .spanish import Spanish
+from .urdu import Urdu
+class LanguageClient:
+    chiness = Chiness
+    english = English
+    spanish = Spanish
+    urdu = Urdu
---
+++ Factory/example-01/lang_client/chinese.py
@@ -0,0 +1,7 @@
+class Chiness:
+    def __init__(self, creds):
+        if creds != "chinese":
+            raise Exception("Invalide creds")
+        
+    def translation(self):
+        return "From Chines Language"
